0c15604c2c7d08c52bb0120dac0e5deefa41b674
Muhambet <muhambet0772218904@gmail.com>
2023-05-11 20:40:17 +0600

    socks

---
+++ src/redux/mens/socks/SocksData.js
@@ -0,0 +1,603 @@
+const socks = [{
+        id: 201,
+        name: 'Nike Everyday Plus Cushioned',
+        title: 'Training Crew Socks (3 Pairs)',
+        price: 18,
+        img: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/402babc5-e333-47d2-b940-0fedf6bc8b0d/everyday-plus-cushioned-training-crew-socks-3-pairs-kt8drs.png',
+        description: 'The Nike Everyday Plus Cushioned Socks bring comfort to your workout with extra cushioning under the heel and forefoot and a snug, supportive arch band. Sweat-wicking power and breathability up top help keep your feet dry and cool to help push you through that extra set.',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 202,
+        name: 'Nike Everyday Plus Cushioned',
+        title: 'Training Crew Socks (6 Pairs)',
+        price: 22,
+        img: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/ef393b64-844d-4d3e-97d8-ddd15819b5af/everyday-plus-cushioned-training-crew-socks-6-pairs-vlRw4Q.png',
+        description: 'The Nike Everyday Plus Cushioned Socks bring comfort to your workout with extra cushioning under the heel and forefoot and a snug, supportive arch band. Sweat-wicking power and breathability up top help keep your feet dry and cool to help push you through that extra set.',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 203,
+        name: 'Nike Everyday Plus Cushioned',
+        title: 'Training Ankle Socks (6 Pairs)',
+        price: 22,
+        img: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/e74435a1-6f46-45bb-9d00-d8ee674f43f4/everyday-plus-cushioned-training-ankle-socks-6-pairs-xql9Hn.png',
+        description: 'The Nike Everyday Plus Cushioned Socks bring comfort to your workout with extra cushioning under the heel and forefoot and a snug, supportive arch band. Sweat-wicking power and breathability up top help keep your feet dry and cool to help push you through that extra set.',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 204,
+        name: 'Nike Everyday Plus Cushioned',
+        title: 'Training No-Show Socks (6 Pairs)',
+        price: 22,
+        img: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/e14330cb-6673-42ae-b4f1-074adbd6b68a/everyday-plus-cushioned-training-no-show-socks-6-pairs-XXM9xm.png',
+        description: 'The Nike Everyday Plus Cushioned Socks bring comfort to your workout with extra cushioning under the heel and forefoot and a snug, supportive arch band. Sweat-wicking power and breathability up top help keep your feet dry and cool to help push you through that extra set.',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 205,
+        name: 'Nike Everyday Plus',
+        title: 'Cushioned Crew Socks',
+        price: 12,
+        img: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/864d5cbc-ad5a-44c5-a278-84b76254bd83/everyday-plus-cushioned-crew-socks-1fTbZX.png',
+        description: 'A true hybrid of style and comfort, the Nike Everyday Plus Crew Socks are cushioned in keys zones to keep you comfortable throughout the day. Dri-FIT fabric and breathable mesh at the top of your foot help keep your feet feeling cool and dry while an arch band provides a snug fit.',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 206,
+        name: 'Nike Everyday Plus',
+        title: 'Cushioned Tie-Dye Crew Socks (2 Pairs)',
+        price: 18,
+        img: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/0490c3c0-af3a-4ad2-b561-8a364cd11dad/everyday-plus-cushioned-tie-dye-crew-socks-2-pairs-cs7hcq.png',
+        description: 'A true hybrid of style and comfort, the Nike Everyday Plus Crew Socks are cushioned in key areas to keep you comfortable throughout the day. Dri-FIT technology and breathable mesh at the top of your foot help keep your feet feeling cool and dry while an arch band provides a snug fit.',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 207,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 208,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 209,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 210,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 211,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 212,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 213,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 214,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 215,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 216,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 217,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 218,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 219,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+    {
+        id: 220,
+        name: '',
+        title: '',
+        price: ,
+        img: '',
+        description: '',
+        size: [{
+            id: 1,
+            s: 'XS'
+        }, {
+            id: 2,
+            s: 'S'
+        }, {
+            id: 3,
+            s: 'M'
+        }, {
+            id: 4,
+            s: 'L'
+        }, {
+            id: 5,
+            s: 'XL'
+        }, {
+            id: 6,
+            s: '2XL'
+        }, {
+            id: 7,
+            s: '3XL'
+        }],
+    },
+]
+
+export default socks
